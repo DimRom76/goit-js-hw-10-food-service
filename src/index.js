871dac652c3172484352b6f03074fcf2a5d0cb23
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2020-11-19 08:33:35 +0200

    меню с фильтром

--- src/index.js
+++ src/index.js
@@ -1,1 +1,98 @@
 import './styles.css';
+import menuTemplates from './templates/item.hbs';
+import ingradientTemplates from './templates/ingradients.hbs';
+import itemMenu from './menu.json';
+
+const STORAGE_THEME = 'theme';
+const ALL_INGRADIENTS = 'Все';
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+  switcherTheme: document.getElementById('theme-switch-toggle'),
+  listMenu: document.querySelector('.js-menu'),
+  listIngradienst: document.querySelector('.js-ingradients'),
+  body: document.querySelector('body'),
+  menuContainer: document.querySelector('.menu-container'),
+};
+
+refs.switcherTheme.addEventListener('change', onChangeTheme);
+refs.menuContainer.addEventListener('click', onClickMenuContainer);
+
+function setStorageTheme(currentTheme) {
+  localStorage.setItem(STORAGE_THEME, currentTheme);
+}
+
+function setTheme(darkThem) {
+  //сбросим все классы на боди
+  for (let key in Theme) {
+    refs.body.classList.remove(Theme[key]);
+  }
+
+  const currentTheme = darkThem ? Theme.DARK : Theme.LIGHT;
+
+  refs.body.classList.add(currentTheme);
+  setStorageTheme(currentTheme);
+}
+
+function onChangeTheme(e) {
+  setTheme(e.target.checked);
+}
+
+function getStorageTheme() {
+  if (localStorage.getItem(STORAGE_THEME) === Theme.DARK) {
+    refs.switcherTheme.checked = true;
+  }
+  setTheme(refs.switcherTheme.checked);
+}
+
+getStorageTheme();
+
+function onClickMenuContainer(e) {
+  const filterMenu = e.target;
+  if (filterMenu.nodeName !== 'LI') {
+    return;
+  }
+
+  setMenuByFilter(filterMenu.textContent);
+}
+
+function setMenuByFilter(filterMenu) {
+  let currentMenu = [];
+  if (filterMenu === ALL_INGRADIENTS) {
+    currentMenu = itemMenu.slice();
+  } else {
+    currentMenu = itemMenu.filter(el => {
+      if (el.ingredients.includes(filterMenu)) {
+        return true;
+      }
+      return false;
+    });
+  }
+
+  const menu = menuTemplates(currentMenu);
+  refs.listMenu.innerHTML = '';
+  refs.listMenu.insertAdjacentHTML('beforeend', menu);
+
+  setFilterMenu(currentMenu);
+}
+
+function setFilterMenu(currentMenu) {
+  const ingradients = currentMenu
+    .reduce((newArray, { ingredients }) => {
+      const newIngradient = ingredients.filter(el => !newArray.includes(el));
+      return [...newArray, ...newIngradient];
+    }, [])
+    .sort();
+
+  const ingradientsFilter = ingradientTemplates([
+    ALL_INGRADIENTS,
+    ...ingradients,
+  ]);
+  refs.listIngradienst.innerHTML = '';
+  refs.listIngradienst.insertAdjacentHTML('beforeend', ingradientsFilter);
+}
+
+setMenuByFilter(ALL_INGRADIENTS);
